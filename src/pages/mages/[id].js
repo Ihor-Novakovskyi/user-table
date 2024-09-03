9f177ce2ca518b9a5419800b2c612f928a9ff4da
Igor <igorplan3@gmail.com>
2024-09-03 20:12:02 +0300

    delete extra files

--- src/pages/mages/[id].js
+++
@@ -1,7 +0,0 @@
-import { useRouter, useSearchParams } from 'next/router'
-export default function Show({ params }) {
-    const router = useRouter()
-    return (<h1>
-        Hello -  {router.query.id}   
-    </h1>)
-}
--- src/pages/rage/index.js
+++
@@ -1,6 +0,0 @@
-export default function Rage({ params }) { 
-    // console.log(params.id)
-    return (
-    <h1>Rage</h1>
-)
-}
